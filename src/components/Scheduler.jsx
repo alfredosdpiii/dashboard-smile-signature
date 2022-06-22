a8cf0cf041a3b1ebb46f5963f031792cc7e9166b
Ronny <91857581+macarron001@users.noreply.github.com>
2022-06-22 16:11:58 +0800

    added calendar component with header and sidebar placeholder

---
+++ src/components/Scheduler.jsx
@@ -0,0 +1,58 @@
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import enUS from 'date-fns/locale/en-US'
+import logo1 from '../logo1.png'
+import logo2 from '../logo2.png'
+
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+
+
+const Scheduler = () => {
+
+  return (
+    <>
+      <div className="grid grid-cols-[15%_85%] min-h-screen">
+        <div className="border">SIDEBAR</div>
+        <div className="flex flex-col">
+          <div className="flex justify-between h-20 border-y-2 px-5">
+            <div>
+              <img src={logo2} width="80" height="50" />
+            </div>
+            <div className="flex items-center">
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                Set an appointment
+              </button>
+            </div>
+          </div>
+          <div>
+            <Calendar
+            localizer={localizer}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: '55em', width: '101em', margin: "5px" }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Scheduler
