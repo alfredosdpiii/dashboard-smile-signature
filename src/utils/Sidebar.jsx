4a72c89ea490a10b498c7962456be28bfffc2463
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-22 23:10:27 +0800

    changed to justify-start

--- src/utils/Sidebar.jsx
+++ src/utils/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   return (
     <aside className="w-{38} h-screen">
       <div className="sticky top-0 p-4 w-full h-screen">
-        <div className="flex flex-col overflow-hidden justify-center align-middle h-screen">
+        <div className="flex flex-col overflow-hidden justify-start align-middle h-screen">
           <div className="items-center">
             <img src={logo2} width="80" height="80" />
           </div>
