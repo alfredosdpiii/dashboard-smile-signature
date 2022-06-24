0c8daacc31ec36d282257f3b9cde822609f009ad
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-24 19:59:39 +0800

    🚧 Fix

--- src/components/PatientRegister.jsx
+++ src/components/PatientRegister.jsx
@@ -43,9 +43,9 @@ const PatientRegister = ({ setIsAddingPatient }) => {
                     </option>
                   </select>
                 </div>
-                <div className='flex flex-col text-black py-2' onClick={()=> setShowCalendar(!showCalendar)}>
+                <div className='flex flex-col text-black py-2'>
                   <label className='black'>Date of Birth</label>
-                  <input className='rounded-lg bg-gray-200 mt-2 p-2 focus:border-blue-500 focus:bg-gray-600 focus:outline-none' type={'text'} onChange={setSelectedDay} value={selectedDay}/>
+                    <input className='rounded-lg bg-gray-200 mt-2 p-2 focus:border-blue-500 focus:bg-gray-600 focus:outline-none' type={'text'} onChange={setSelectedDay} value={selectedDay}/>
                   {showCalendar && (
                   <Calendar onChange={setSelectedDay} value={selectedDay} className='w-[27.3em] rounded-lg absolute bottom-[118px]'/>
                   )}
