6498a5eaa956e3da47f267c207e3e94cfc8869f7
Ronny <91857581+macarron001@users.noreply.github.com>
2022-06-28 23:45:57 +0800

    edited some styles in staff profile modal

--- src/components/StaffProfile.jsx
+++ src/components/StaffProfile.jsx
@@ -28,37 +28,40 @@ const StaffProfile = ({ setIsSelectingStaff, staff }) => {
       <div className="justify-center items-center flex overflow-x-hidden fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="w-[30em] relative my-8 mx-auto">
           <div className='w-full bg-white rounded-lg'>
-            <div className='w-full h-10 flex flex-row justify-start text-center items-center py-8 px-5 border-b border-solid border-slate-200 rounded-b'>
+            <div className='w-full h-10 flex flex-row justify-start text-center items-center py-8 px-5 border-b border-solid border-slate-200 bg-zinc-100 rounded-b'>
               <img src={person} alt='' className='object-scale-down h-10 w-10 mr-3' />
               <h1 className='font-medium text-lg'>{(`${profile.first_name} ${profile.last_name}`).toUpperCase()}</h1>
             </div>
             <div className='w-full h-full flex flex-col'>
               <div className='flex justify-center items-center px-10 py-10'>
+                <div>
                 <img src={staff.imgAvatar} alt='' className='object-scale-down h-[13em] w-[13em] mx-auto rounded-[50%]' />
+                </div>
+                
               </div>
-              <div className='w-full text-center py-2'>
+              <div className='w-full text-center py-2 bg-zinc-50'>
                 <h1 className='text-3xl font-bold py-1'>{profile.first_name} {profile.last_name}</h1>
-                <p className='text-xl'>Professional Dentist</p>
+                <p className='text-xl'>{profile.position}</p>
                 <p className='text-xl text-gray-500'>{staff.email}</p>
               </div>
-              <div className='w-full text-center px-5 py-2'>
+              <div className='w-full text-center px-5 py-2 bg-zinc-50'>
                 <h3 className='text-base font-medium'>Gender</h3>
                 <p className='text-base text-gray-500'>{Capitalize(staff.gender)}</p>
               </div>
-              <div className='w-full text-center px-5 py-2'>
+              <div className='w-full text-center px-5 py-2 bg-zinc-50'>
                 <h3 className='text-base font-medium'>Date of Birth</h3>
                 <p className='text-base text-gray-500'>{dayjs(profile.date_of_birth).format('MMMM DD, YYYY')}</p>
               </div>
-              <div className='w-full text-center px-5 py-2'>
+              <div className='w-full text-center px-5 py-2 bg-zinc-50'>
                 <h3 className='text-base font-medium'>Mobile</h3>
                 <p className='text-base text-gray-500'>{profile.mobile}</p>
               </div>
-              <div className='w-full text-center px-5 py-2 mb-5'>
+              <div className='w-full text-center px-5 py-2 bg-zinc-50 pb-5'>
                 <h3 className='text-base font-medium'>Address</h3>
                 <div className='text-base text-gray-500'>{profile.address}</div>
               </div>
             </div>
-            <div className="flex items-center justify-end p-3 border-t border-solid border-slate-200 rounded-b">
+            <div className="flex items-center justify-end p-3 border-t border-solid border-slate-300 bg-zinc-100 rounded-b">
               <button
                 className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
