4710a80bca8ca3e7e4d4e2ad1ea0096f9dd492f3
Ronny <91857581+macarron001@users.noreply.github.com>
2022-06-29 01:01:45 +0800

    added login functionality using axios

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,84 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import logo1 from '../assets/images/logo1.png'
 import background from '../assets/images/background1.jpg'
+import { useForm } from 'react-hook-form';
+import axios from 'axios'
+
 
 const Login = () => {
+  const [formError, setFormError] = useState(false);
+
+  const { 
+    register, 
+    handleSubmit, 
+    getValues,
+    formState: { errors, isSubmitSuccessful },
+    setError
+  } = useForm({});
+
+  useEffect(() => {
+    if(isSubmitSuccessful) {
+      const values = getValues();
+
+      axios({
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        url: 'http://localhost:3001/login',
+        data:{"user":{
+          "email": values.email,
+          "password": values.password
+          }
+        } 
+      })
+      .then((res) => {
+          console.log(values)
+          console.log(res)
+        })
+      .catch((error) => {
+        if (error) {
+          setFormError(true)
+        }
+      })
+    }
+  },[isSubmitSuccessful])
+
+  const onSubmit = (data) => {
+   console.log(data)
+  }
+
   return (
     <>
       <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
         <div className='hidden sm:block'>
-          <img className='w-full h-[975px] object-cover' src={background} alt='' />
+          <img className='w-full h-screen object-cover' src={background} alt='' />
         </div>
 
         <div className='bg-neutral-200 flex flex-col justify-center'>
-          <form className='max-w-[400px] w-full mx-auto bg-white p-8 px-8 rounded-lg'>
+          <form className='max-w-[400px] w-full mx-auto bg-white p-8 px-8 rounded-lg' onSubmit={handleSubmit(onSubmit)}>
             <img className='object-scale-down h-21 w-40 mx-auto' src={logo1} alt='' />
             <div className='flex flex-col text-black py-2'>
               <label className='black'>Email</label>
-              <input className='rounded-lg bg-gray-200 mt-2 p-2 focus:border-blue-500 focus:bg-gray-600 focus:outline-none' type="text" />
+              <input 
+              className='rounded-lg bg-gray-200 mt-2 p-2 focus:border-blue-500 focus:bg-gray-600 focus:outline-none focus:text-white' 
+              type="text"
+              {...register("email", {required: "This is required"})}
+              />
+              <span className='text-red-600'>{errors.email?.message}</span>
             </div>
             <div className='flex flex-col text-black py-2'>
               <label>Password</label>
-              <input className='rounded-lg bg-gray-200 mt-2 p-2 focus:border-blue-500 focus:bg-gray-600 focus:outline-none' type="password" />
+              <input 
+              className='rounded-lg bg-gray-200 mt-2 p-2 focus:border-blue-500 focus:bg-gray-600 focus:outline-none focus:text-white' 
+              type="password" 
+              {...register("password", {required: "This is required"})}
+              />
+              <span className='text-red-600'>{errors.password?.message}</span>
             </div>
+            {formError && (
+              <span className='text-red-600'>
+                Incorrect email or password
+              </span>
+            )}
             <div className='flex justify-between text-black py-2'>
               <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
               <p>Forgot Password</p>
